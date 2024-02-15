40c7421227eb2f5f901586cddc6a6bc8ea5a961c
codenaud <codenaud@gmail.com>
2024-02-15 10:17:18 +0100

    basic routes & initial configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MapComponent } from './pages/map/map.component';
+import { GraphicsComponent } from './pages/graphics/graphics.component';
+import { FullcalendarComponent } from './pages/fullcalendar/fullcalendar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'map', component: MapComponent },
+  { path: 'graphics', component: GraphicsComponent },
+  { path: 'fullcalendar', component: FullcalendarComponent },
+  // { path: '**', component: PageNotFoundComponent },
+];
