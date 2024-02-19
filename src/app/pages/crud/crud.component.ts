77f461ea5eaa4f51a169eabc039539ebd419ac4e
codenaud <codenaud@gmail.com>
2024-02-19 21:24:41 +0100

    First steps on crud page

--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { NavComponent } from '../../shared/components/header/nav/nav.component';
 
 @Component({
   selector: 'app-crud',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterOutlet],
+  imports: [CommonModule, RouterLink, RouterOutlet, NavComponent],
   templateUrl: './crud.component.html',
   styleUrl: './crud.component.scss',
 })
