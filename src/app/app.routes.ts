f0cf10e773fd20f2a05f3ccd75fe61b017368f02
codenaud <codenaud@gmail.com>
2024-02-19 12:14:24 +0100

    installing coreui in angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { MapComponent } from './pages/map/map.component';
 import { GraphicsComponent } from './pages/graphics/graphics.component';
 import { FullcalendarComponent } from './pages/fullcalendar/fullcalendar.component';
+import { UserComponent } from './pages/user/user.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'user', component: UserComponent },
   { path: 'map', component: MapComponent },
   { path: 'graphics', component: GraphicsComponent },
   { path: 'fullcalendar', component: FullcalendarComponent },
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/components/header/header.component';
+import { SliderComponent } from '../../shared/components/slider/slider.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SliderComponent],
 })
 export class HomeComponent {}
