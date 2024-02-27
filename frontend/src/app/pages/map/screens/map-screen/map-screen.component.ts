78498a55a06fb5c154d3fac82cfb0a215a4874d0
codenaud <codenaud@gmail.com>
2024-02-27 18:11:49 +0100

    Search Bar & Seaarch Results Components

--- frontend/src/app/pages/map/screens/map-screen/map-screen.component.ts
+++ frontend/src/app/pages/map/screens/map-screen/map-screen.component.ts
@@ -7,6 +7,7 @@ import { LoadingComponent } from '../../components/loading/loading.component';
 import { MapViewComponent } from '../../components/map-view/map-view.component';
 import { AngularLogoComponent } from '../../components/angular-logo/angular-logo.component';
 import { BtnMyLocationComponent } from '../../components/btn-my-location/btn-my-location.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-map-screen',
@@ -21,6 +22,7 @@ import { BtnMyLocationComponent } from '../../components/btn-my-location/btn-my-
     MapViewComponent,
     AngularLogoComponent,
     BtnMyLocationComponent,
+    SearchBarComponent,
   ],
 })
 export class MapScreenComponent {
