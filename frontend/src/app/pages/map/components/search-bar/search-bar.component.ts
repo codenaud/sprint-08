924fb90af4bd7ae5c039281f860b8e217324d215
codenaud <codenaud@gmail.com>
2024-02-27 19:19:44 +0100

    Search bar & search results styles

--- frontend/src/app/pages/map/components/search-bar/search-bar.component.ts
+++ frontend/src/app/pages/map/components/search-bar/search-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { SearchResultsComponent } from '../search-results/search-results.component';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
   templateUrl: './search-bar.component.html',
   styleUrl: './search-bar.component.scss',
+  imports: [SearchResultsComponent],
 })
 export class SearchBarComponent {}
