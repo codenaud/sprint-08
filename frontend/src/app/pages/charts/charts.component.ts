1b214a77e298dfceefb58bf46997f0db0de43d2e
codenaud <codenaud@gmail.com>
2024-03-05 11:38:28 +0100

    connect angular with hardcode chart.js

--- frontend/src/app/pages/charts/charts.component.ts
+++ frontend/src/app/pages/charts/charts.component.ts
@@ -5,6 +5,9 @@ import { HeaderComponent } from '../../shared/components/header/header.component
 import { HeroComponent } from '../../shared/components/hero/hero.component';
 import { Hero } from '../../interfaces/hero';
 import { HeroService } from '../../services/hero.service';
+import { Chart, registerables } from 'chart.js/auto';
+
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-charts',
@@ -29,5 +32,46 @@ export class ChartsComponent implements OnInit {
 
   ngOnInit(): void {
     // Ahora crudHero se obtiene del servicio
+    this.RenderChart();
+  }
+
+  RenderChart() {
+    const ctx = document.getElementById('myChart');
+
+    new Chart('piechart', {
+      type: 'bar',
+      data: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+        ],
+        datasets: [
+          {
+            label: '# My First dataset',
+            data: [65, 60, 80, 82, 55, 55, 40],
+            borderWidth: 1,
+            backgroundColor: ['rgb(66, 145, 245)'],
+          },
+          {
+            label: '# My Second dataset',
+            data: [25, 50, 40, 20, 85, 25, 90],
+            borderWidth: 1,
+            backgroundColor: ['rgb(255, 167, 38)'],
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
 }
